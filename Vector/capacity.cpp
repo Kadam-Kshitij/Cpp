330e00805d1e3bf6160b7ba61eb9826cb77dc27f
Kshitij Kadam <54320682+Kadam-Kshitij@users.noreply.github.com>
2022-07-21 22:01:21 +0530

    Update capacity.cpp

--- Vector/capacity.cpp
+++ Vector/capacity.cpp
@@ -8,10 +8,14 @@ int main()
 
 	v.push_back( 12 );
 	std::cout << "Vector capacity, size = " << v.capacity() << " " << v.size() << std::endl;
+	
+	v.push_back( 12 );
+	std::cout << "Vector capacity, size = " << v.capacity() << " " << v.size() << std::endl;
 }
 
 // Output
 // Vector capacity, size = 6 6
 // Vector capacity, size = 12 7
+// Vector capacity, size = 12 8
 
 // Capacity doubles everytime a reallocation of vector happens
