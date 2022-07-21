ae662da9dbe461d74384913870358c3756fecb95
Kshitij Kadam <54320682+Kadam-Kshitij@users.noreply.github.com>
2022-07-21 22:00:08 +0530

    Create capacity.cpp

---
+++ Vector/capacity.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+	std::vector< int > v = { 1, 2, 3, 4, 5, 6 };
+	std::cout << "Vector capacity, size = " << v.capacity() << " " << v.size() << std::endl;
+
+	v.push_back( 12 );
+	std::cout << "Vector capacity, size = " << v.capacity() << " " << v.size() << std::endl;
+}
+
+// Output
+// Vector capacity, size = 6 6
+// Vector capacity, size = 12 7
+
+// Capacity doubles everytime a reallocation of vector happens
